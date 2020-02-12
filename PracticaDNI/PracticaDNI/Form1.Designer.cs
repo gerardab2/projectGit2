ebe5d17530219b783c86b7d63f7ea4f21f42c987
“gerardab2” <gerard.amirian@hotmail.com>
2020-02-12 13:07:57 +0100

    Version 0.0.1

---
+++ PracticaDNI/PracticaDNI/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PracticaDNI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.textBoxDNI = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxLettet = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOK.Location = new System.Drawing.Point(144, 146);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(233, 48);
+            this.buttonOK.TabIndex = 0;
+            this.buttonOK.Text = "button1";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // textBoxDNI
+            // 
+            this.textBoxDNI.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDNI.Location = new System.Drawing.Point(144, 51);
+            this.textBoxDNI.Name = "textBoxDNI";
+            this.textBoxDNI.Size = new System.Drawing.Size(193, 34);
+            this.textBoxDNI.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "DNI";
+            // 
+            // textBoxLettet
+            // 
+            this.textBoxLettet.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLettet.Location = new System.Drawing.Point(343, 51);
+            this.textBoxLettet.Name = "textBoxLettet";
+            this.textBoxLettet.Size = new System.Drawing.Size(34, 34);
+            this.textBoxLettet.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 293);
+            this.Controls.Add(this.textBoxLettet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDNI);
+            this.Controls.Add(this.buttonOK);
+            this.Name = "Form1";
+            this.Text = "FormDNI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.TextBox textBoxDNI;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxLettet;
+    }
+}
+
---
+++ PracticaDNI/PracticaDNI/Form1.cs
@@ -0,0 +1,35 @@
+﻿//12/02/2020
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaDNI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            int dni =Convert.ToInt32(textBoxDNI.Text);
+            char letter = functionLetter(dni);
+            textBoxLettet.Text = letter.ToString();
+            MessageBox.Show("done");
+        }
+        private char functionLetter(int dni)
+        {
+            char letter = 'A';
+
+            return letter;
+        }
+    }
+}
